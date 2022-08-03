b670a3de0bdf586eeca77856e5056a3b679cd7af
Arif5577 <arifalamm33@gmail.com>
2022-08-03 12:05:19 +0600

    profile page added

--- client/src/components/pages/Dashboard/Admit/Admit.jsx
+++ client/src/components/pages/Dashboard/Admit/Admit.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
 const Admit = () => {
-  return <div>Admit Student</div>;
+  return (
+    <div>
+      <div className="p-5 bg-slate-900 m-3 rounded">
+        <img
+          src="images/Logo.svg"
+          alt="Logo"
+          className="w-20 border rounded-full p-2 h-20 mx-auto"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Admit;
--- client/src/components/pages/Dashboard/index.jsx
+++ client/src/components/pages/Dashboard/index.jsx
@@ -11,43 +11,46 @@ import {
 const Dashboard = () => {
   return (
     <section className="min-h-screen">
-      <div className="grid grid-cols-12 gap-5">
-        <div className="md:col-span-3  col-span-4 bg-slate-900 min-h-screen">
+      <div className="grid grid-cols-12 lg:gap-5">
+        <div className="lg:col-span-3 col-span-2 bg-slate-900 min-h-screen">
           <div className="my-5">
             <img
               src="images/admin-mask.svg"
               alt="admin  "
-              className="h-20 w-20 mx-auto  "
+              className="lg:h-20 lg:w-20 h-10 w-10 mx-auto  "
             />
 
             <div>
-              <h2 className="uppercase text-2xl font-bold text-center my-3 text-white hidden md:block">
+              <h2 className="uppercase text-2xl font-bold text-center my-3 text-white hidden lg:block">
                 MD ARIFUL ISLAM
               </h2>
 
-              <ul className="mt-4 flex flex-col items-center gap-0">
+              <ul className="mt-4 justify-center text-center  flex flex-col items-center ">
                 <Link
                   to="/dashboard"
-                  className="p-4 bg-primary text-white uppercase font-bold hover:bg-slate-900 duration-200 w-full text-left pl-12 flex items-center gap-1"
-                  Profile
+                  className="p-4 bg-primary text-white uppercase font-bold hover:bg-primary  duration-200 w-full text-left lg:pl-12 pl-0 flex items-center lg:gap-1 gap-0 lg:justify-start justify-center  hover:translate-x-1"
                 >
-                  <User size={20} color="#fff" />
+                  <User size={20} className="ml-4 sm:ml-0 " color="#fff" />
                   <span className="hidden lg:block">Profile</span>
                 </Link>
 
                 <Link
-                  className="p-4 bg-slate-900 text-white uppercase font-bold hover:bg-primary duration-200 w-full text-left pl-12 flex items-center gap-1"
+                  className="p-4 bg-slate-900 text-white uppercase font-bold hover:bg-primary duration-200 w-full text-left lg:pl-12 pl-0 flex items-center lg:gap-1 gap-0 lg:justify-start justify-center hover:translate-x-1"
                   to="/dashboard/admit"
                 >
-                  <Student size={20} color="#fff" />
+                  <Student size={20} className="ml-4 sm:ml-0 " color="#fff" />
                   <span className="hidden lg:block">Admit Student</span>
                 </Link>
 
                 <Link
-                  className="p-4 bg-slate-900 text-white uppercase font-bold hover:bg-primary duration-200 w-full text-left pl-12 flex items-center gap-1 dropdown dropdown-right"
+                  className="p-4 bg-slate-900 text-white uppercase font-bold hover:bg-primary duration-200 w-full text-left lg:pl-12 pl-0 flex items-center lg:gap-1 gap-0 lg:justify-start justify-center dropdown dropdown-right hover:translate-x-1"
                   to="#"
                 >
-                  <ChalkboardSimple size={20} color="#fff" />
+                  <ChalkboardSimple
+                    size={20}
+                    className="ml-4 sm:ml-0 "
+                    color="#fff"
+                  />
                   <span className="hidden lg:block">Classes</span>
                   <ul
                     tabindex="0"
@@ -72,33 +75,37 @@ const Dashboard = () => {
                 </Link>
 
                 <Link
-                  className="p-4 bg-slate-900 text-white uppercase font-bold hover:bg-primary duration-200 w-full text-left pl-12 flex items-center gap-1"
+                  className="p-4 bg-slate-900 text-white uppercase font-bold hover:bg-primary duration-200 w-full text-left lg:pl-12 pl-0 flex items-center lg:gap-1 gap-0 lg:justify-start justify-center hover:translate-x-1"
                   to="/dashboard/teachers"
                 >
-                  <ChalkboardTeacher size={20} color="#fff" />
+                  <ChalkboardTeacher
+                    size={20}
+                    className="ml-4 sm:ml-0 "
+                    color="#fff"
+                  />
                   <span className="hidden lg:block">Teachers</span>
                 </Link>
 
                 <Link
-                  className="p-4 bg-slate-900 text-white uppercase font-bold hover:bg-primary duration-200 w-full text-left pl-12 flex items-center gap-1"
+                  className="p-4 bg-slate-900 text-white uppercase font-bold hover:bg-primary duration-200 w-full text-left lg:pl-12 pl-0 flex items-center lg:gap-1 gap-0 lg:justify-start justify-center hover:translate-x-1"
                   to="/dashboard/actives"
                 >
-                  <Activity size={20} color="#fff" />
+                  <Activity size={20} className="ml-4 sm:ml-0 " color="#fff" />
                   <span className="hidden lg:block">Actives</span>
                 </Link>
 
                 <Link
-                  className="p-4 bg-slate-900 text-white uppercase font-bold hover:bg-primary duration-200 w-full text-left pl-12 flex items-center gap-1"
+                  className="p-4 bg-slate-900 text-white uppercase font-bold hover:bg-primary duration-200 w-full text-left lg:pl-12 pl-0 flex items-center lg:gap-1 gap-0 lg:justify-start justify-center hover:translate-x-1"
                   to="/dashboard/policy"
                 >
-                  <Lock size={20} color="#fff" />
+                  <Lock size={20} className="ml-4 sm:ml-0 " color="#fff" />
                   <span className="hidden lg:block">Policy</span>
                 </Link>
               </ul>
             </div>
           </div>
         </div>
-        <div className="md:col-span-9 col-span-8 min-h-screen">
+        <div className="lg:col-span-9 col-span-10 min-h-screen">
           <Outlet />
         </div>
       </div>
